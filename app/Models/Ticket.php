5d978b4d83dc28a942aaed23d92a8800f12cee8b
Jaime García <jaime.garcia@iesdonana.org>
2025-02-18 20:12:58 +0100

    Migraciones y relaciones listas

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Producto extends Model
+class Ticket extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProductoFactory> */
+    /** @use HasFactory<\Database\Factories\TicketFactory> */
     use HasFactory;
     use SoftDeletes;
 
-    public function lineas()
+    public function tickets()
     {
         return $this->hasMany(Linea::class);
     }
